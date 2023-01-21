b75710ce96a14e102055e4cc82f779814333a9c4
Sandeep Dash <contactsandeepdash@gmail.com>
2023-01-21 23:47:24 +0530

    added actuator

--- employee-service/src/main/java/com/dash/employeeservice/controller/EmployeeController.java
+++ employee-service/src/main/java/com/dash/employeeservice/controller/EmployeeController.java
@@ -1,6 +1,5 @@
 package com.dash.employeeservice.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +9,6 @@ public class EmployeeController {
 
     @GetMapping(value = "/getEmployeeList")
     public ResponseEntity<String> getEmployeeList() {
-        return new ResponseEntity<>(
-                "get List of employees",
-                HttpStatus.OK);
+        return ResponseEntity.ok("get List of employees");
     }
 }
