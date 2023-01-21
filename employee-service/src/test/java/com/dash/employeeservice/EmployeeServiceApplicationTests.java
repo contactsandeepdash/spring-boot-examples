fa9558a853b3c4f3b1f2b6b35e0416105193f552
Sandeep Dash <contactsandeepdash@gmail.com>
2023-01-21 19:19:07 +0530

    init employee-service

---
+++ employee-service/src/test/java/com/dash/employeeservice/EmployeeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dash.employeeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
