4884d55ffc7ae1af189b74c0ae1fd20c0131aade
Sandeep Dash <contactsandeepdash@gmail.com>
2023-01-22 22:23:51 +0530

    initial commit for work experience service

---
+++ work-experience-service/src/main/java/com/dash/workexperienceservice/WorkExperienceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dash.workexperienceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkExperienceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkExperienceServiceApplication.class, args);
+	}
+
+}
---
+++ work-experience-service/src/test/java/com/dash/workexperienceservice/WorkExperienceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dash.workexperienceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkExperienceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
