bd4d1239b7d0a9d26774dec76560fa86d90742c1
Sandeep Dash <contactsandeepdash@gmail.com>
2023-02-04 18:45:58 +0530

    choice and when operators

--- camel-microservice/src/main/java/com/dash/camelmicroservice/router/MyFileRouter.java
+++ camel-microservice/src/main/java/com/dash/camelmicroservice/router/MyFileRouter.java
@@ -3,13 +3,30 @@ package com.dash.camelmicroservice.router;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
+
+// https://camel.apache.org/components/3.20.x/languages/simple-language.html
+// https://camel.apache.org/components/3.20.x/index.html
+
 @Component
 public class MyFileRouter extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
         from("file:/Users/sandeepdash/Desktop/source")
-        .log("${body}")
+        .routeId("files-input-route")
+        .transform().body(String.class)
+        .choice()
+            .when(simple("${file:ext} ends with 'xml'"))
+                .log("XML File")
+                .when(simple("${body} contains 'USD'"))
+                    .log("Not a XML file; but, contains USD")
+            .otherwise()
+                .log("not a XML File")
+        .end()
+        .log("${messageHistory} : ${headers.CamelFileAbsolute}")
+        .log("${file:absolute.path} : ${file:name} : ${file:name.ext} : ${file:name.noext} : ${file:onlyname} : ${file:onlyname.noext}")
+        .log("${file:parent} : ${file:path} : ${file:absolute} : ${file:size} : ${file:modified}")
+        .log("${routeId} : ${camelId} : \n${body}")
         .to("file:/Users/sandeepdash/Desktop/target");
         
     }
